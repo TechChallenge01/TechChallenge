054dec449fd2c8c5f28acf753ab0be04991d488b
Joao Freittas <joaofreittas461@gmail.com>
2026-04-08 20:00:21 -0300

    feat: validar duplicidade de email, telefone, endereço

--- Domain/Entities/Cliente.cs
+++ Domain/Entities/Cliente.cs
@@ -41,18 +41,39 @@ namespace Domain.Entities
         public void AdicionarEndereco(Endereco endereco)
         {
             if(endereco == null) throw new ArgumentNullException(nameof(endereco)) ;
+
+            bool enderecoExistente = _enderecos.Any(enderecoTemporario => 
+                enderecoTemporario.Logradouro.Equals(endereco.Logradouro, StringComparison.OrdinalIgnoreCase) &&
+                enderecoTemporario.Numero.Equals(endereco.Numero, StringComparison.OrdinalIgnoreCase) &&
+                enderecoTemporario.Cep.Equals(endereco.Cep, StringComparison.OrdinalIgnoreCase));
+
+            if (enderecoExistente) { throw new ArgumentException("Este endereço já está cadastrado."); }
+
             _enderecos.Add(endereco);
         }
 
         public void AdicionarEmail(Email email)
         {
             if(email == null) throw new ArgumentNullException(nameof(email)) ;
+
+            bool emailExistente = _emails.Any(emailTemporario => emailTemporario.EnderecoEmail.Equals(email.EnderecoEmail, StringComparison.OrdinalIgnoreCase));
+
+            if(emailExistente) { throw new ArgumentException("Este e-mail já está cadastrado."); }
+
             _emails.Add(email);
         }
 
         public void AdicionarTelefone(Telefone telefone)
         {
             if(telefone == null) throw new ArgumentNullException(nameof(telefone)) ;
+
+            bool telefoneExistente = _telefones.Any(telefoneTemporario => 
+                telefoneTemporario.DDD == telefone.DDD &&
+                telefoneTemporario.DDI == telefone.DDI &&
+                telefoneTemporario.Numero == telefone.Numero);
+
+            if (telefoneExistente) { throw new ArgumentException("Este telefone já está cadastrado."); }
+
             _telefones.Add(telefone);
         }
 
