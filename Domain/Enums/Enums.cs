52c7b4c1800aa53c696478bf1a38303627a66efd
Joao Freittas <joaofreittas461@gmail.com>
2026-04-08 22:00:24 -0300

    feat: implementa enum de tipo de telefone

---
+++ Domain/Enums/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Enums
+{
+    public enum TipoTelefone
+    {
+        Fixo = 1,
+        Celular = 2,
+        Comercial = 3,
+        Residencial = 4,
+        Outro = 5
+    }
+}
