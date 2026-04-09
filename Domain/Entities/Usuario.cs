f152839dab0102d0b57af11ddadb8292fe3d41e6
Joao Freittas <joaofreittas461@gmail.com>
2026-04-08 22:29:56 -0300

    feat: implementa entidade usuario com propriedades basicas

---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Domain.ValueObjects;
+using Domain.Enums;
+using System.Runtime.CompilerServices;
+
+namespace Domain.Entities
+{
+    public class Usuario
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public Email Email { get; private set; }
+        public string SenhaHash { get; private set; }
+        public PerfilUsuario Perfil { get; private set; }
+
+        public Usuario(string name, string email, string senha, PerfilUsuario perfil)
+        {
+            ValidarNome(name);
+            ValidarEmail(email);
+            ValidarSenha(senha);
+
+            Id = Guid.NewGuid();
+            Nome = name;
+            Email = new Email(email);
+            SenhaHash = senha;
+            Perfil = perfil;
+        }
+
+        public Usuario() { }
+
+        private void ValidarNome(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("O nome do usuário não pode ser nulo ou vazio.");
+        }
+
+        private void ValidarEmail(string email) 
+        {
+            Email = new Email(email);
+        }
+
+        private void ValidarSenha(string senha)
+        {
+            if (string.IsNullOrWhiteSpace(senha) || senha.Length > 6 )
+                throw new ArgumentException("A senha deve ter pelo menos 6 caracteres.");
+        }
+    }
+}
