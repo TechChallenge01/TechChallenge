6e0eb35adb02390fb759e7e9dfc699c579137923
Joao Freittas <joaofreittas461@gmail.com>
2026-04-08 22:08:51 -0300

    feat: garantindo imbutabilidade, auto-validacao e igualdade por valor do ValueObjects de endereco

--- Domain/ValueObjects/Endereco.cs
+++ Domain/ValueObjects/Endereco.cs
@@ -6,18 +6,6 @@ namespace Domain.ValueObjects
 {
     public class Endereco
     {
-        public Endereco(string logradouro, string numero, string complemento, string bairro , string cidade, string estado, string cep)
-        {
-            Logradouro = logradouro;
-            Numero = numero;
-            Complemento = complemento;
-            Bairro = bairro;
-            Cidade = cidade;
-            Uf = estado;
-            Cep = cep;
-        }
-
-        protected Endereco() { }
         public string Logradouro { get; private set; }
         public string Numero { get; private set; }
         public string Complemento { get; private set; }
@@ -25,5 +13,44 @@ namespace Domain.ValueObjects
         public string Cep { get; private set; }
         public string Cidade { get; private set; }
         public string Uf { get; private set; }
+
+        public Endereco(string logradouro, string numero, string complemento, string bairro , string cidade, string estado, string cep)
+        {
+            if (string.IsNullOrWhiteSpace(logradouro)) throw new ArgumentException("Logradouro é obrigatório.");
+            if (string.IsNullOrWhiteSpace(numero)) throw new ArgumentException("Número é obrigatório.");
+            if (string.IsNullOrWhiteSpace(cep)) throw new ArgumentException("CEP é obrigatório.");
+            if (string.IsNullOrWhiteSpace(cidade)) throw new ArgumentException("Cidade é obrigatória.");
+            if (string.IsNullOrWhiteSpace(estado) || estado.Length != 2) throw new ArgumentException("UF inválida.");
+
+            Logradouro = logradouro.Trim();
+            Numero = numero.Trim();
+            Complemento = complemento?.Trim() ?? string.Empty;
+            Bairro = bairro?.Trim();
+            Cidade = cidade.Trim();
+            Uf = estado.Trim().ToUpper();
+            Cep = cep.Trim(); 
+        }
+
+        protected Endereco() { }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Endereco outro)
+            {
+                return  Logradouro == outro.Logradouro &&
+                        Numero == outro.Numero &&
+                        Complemento == outro.Complemento &&
+                        Bairro == outro.Bairro &&
+                        Cidade == outro.Cidade &&
+                        Uf == outro.Uf &&
+                        Cep == outro.Cep;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Logradouro.ToLower(), Numero.ToLower(), Complemento.ToLower(), Cep);
+        }
     }
 }
