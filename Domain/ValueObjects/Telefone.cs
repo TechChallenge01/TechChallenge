8d632b6ef9e3dee424ae2cc1f95f43f59ce30d00
Joao Freittas <joaofreittas461@gmail.com>
2026-04-08 22:01:40 -0300

    feat: garantindo imbutabilidade, auto-validacao e igualdade por valor do ValueObjects de telefone

--- Domain/ValueObjects/Telefone.cs
+++ Domain/ValueObjects/Telefone.cs
@@ -1,23 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Domain.Enums;
 
 namespace Domain.ValueObjects
 {
     public class Telefone
     {
-        public Telefone(string ddd, string ddi, string numero, string tipo, Guid clienteId)
-        {
-            DDD = ddd;
-            DDI = ddi;
-            Numero = numero;
-            Tipo = tipo; // Celular, Fixo, etc.
-        }
-
-        protected Telefone() { }
         public string DDD { get; private set; }
         public string DDI { get; private set; }
         public string Numero { get; private set; }
-        public string Tipo { get; private set; }
+        public TipoTelefone Tipo { get; private set; }
+
+        public Telefone(string ddd, string ddi, string numero, TipoTelefone tipo)
+        {
+            if(string.IsNullOrWhiteSpace(ddd))
+                throw new ArgumentException("O DDD não pode ser nulo ou vazio.");
+            if(string.IsNullOrWhiteSpace(ddi))
+                throw new ArgumentException("O DDI não pode ser nulo ou vazio.");
+            if(string.IsNullOrWhiteSpace(numero))
+                throw new ArgumentException("O número não pode ser nulo ou vazio.");
+
+            DDD = ddd.Trim();
+            DDI = ddi.Trim();
+            Numero = numero.Trim();
+            Tipo = tipo; 
+        }
+
+        protected Telefone() { }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Telefone outro) 
+            {
+                return DDI == outro.DDI && 
+                       DDD == outro.DDD && 
+                       Numero == outro.Numero;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(DDI, DDD, Numero);
+        }
+
     }
 }
