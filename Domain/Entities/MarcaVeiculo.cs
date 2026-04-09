e47ea51df2003c4370877fed1a44c894a3db3f3c
Joao Freittas <joaofreittas461@gmail.com>
2026-04-09 19:23:54 -0300

    feat: adiciona entidade MarcaVeiculo
    - Implementa classe herdando de Entity; - Adiciona propriedade Nome para identificação da marca.

---
+++ Domain/Entities/MarcaVeiculo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class MarcaVeiculo : Entity
+    {
+        public string Nome { get; private set; }
+
+        public MarcaVeiculo(string nome, Guid idUsuarioCriacao)
+        {
+            ValidarMarca(nome);
+
+            IdUsuarioCriacao = idUsuarioCriacao;
+            Nome = nome.Trim();
+        }
+
+        protected MarcaVeiculo() { }
+
+        private void ValidarMarca(string nome)
+        {
+            if (string.IsNullOrWhiteSpace(nome))
+                throw new ArgumentException("O nome da marca é obrigatório.");
+        }
+    }
+}
