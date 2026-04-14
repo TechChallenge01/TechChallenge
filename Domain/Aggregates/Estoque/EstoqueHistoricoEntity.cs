f92254f7e479b19974985f36d5650ea57a5957cd
Joao Freittas <joaofreittas461@gmail.com>
2026-04-13 23:11:25 -0300

    feat(domain): adiciona estrutura do agregado de Estoque

---
+++ Domain/Aggregates/Estoque/EstoqueHistoricoEntity.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Aggregates.Estoque;
+
+public class EstoqueHistoricoEntity : Base.BaseEntity
+{
+    public Guid EstoqueId { get; private set; }
+    public int Quantidade { get; private set; } 
+    public string Observacao { get; private set; } = string.Empty;
+    //a ser imprementado codigo da OS
+    public ETipoMovimentacao TipoMovimentacao { get; private set; }
+
+    public EstoqueHistoricoEntity(Guid estoqueId, int quantidade, string observacao, ETipoMovimentacao tipoMovimentacao, Guid UsuarioCriacaoId, DateTime dataCriacao) : base(UsuarioCriacaoId, dataCriacao, null, null)
+    {
+        ValidarEstoqueId(estoqueId);
+        ValidarQuantidade(quantidade);
+
+        EstoqueId = estoqueId;
+        Quantidade = quantidade;
+        Observacao = observacao;
+        TipoMovimentacao = tipoMovimentacao;
+    }
+
+    protected EstoqueHistoricoEntity() { }
+
+    private void ValidarEstoqueId(Guid estoqueId)
+    {
+        if (estoqueId == Guid.Empty)
+            throw new ArgumentException("O Id do estoque não pode ser vazio.");
+    }
+
+    private void ValidarQuantidade(int quantidade)
+    {
+        if (quantidade <= 0)
+            throw new ArgumentException("A quantidade deve ser maior que zero.");
+    }
+}
