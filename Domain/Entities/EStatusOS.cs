9db0d3efe1e695b304762f47c6aaac6475042b72
Joao Freittas <joaofreittas461@gmail.com>
2026-04-14 22:33:33 -0300

    feat: implementa enum EStatusOS
    - Define status: Recebida, Em Diagnóstico, Aguardando Aprovação, Em Execução, Finalizada e Entregue

---
+++ Domain/Entities/EStatusOS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities;
+
+public enum EStatusOS
+{
+    Recebida = 1,
+    EmDiagnostico = 2,
+    AguardandoAprovacao = 3,
+    EmExecucao = 4,
+    Finalizada = 5,
+    Entregue = 6
+}
