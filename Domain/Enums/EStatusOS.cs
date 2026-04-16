c8541e0dee49992072722983ac435e6caa188e66
Joao Freittas <joaofreittas461@gmail.com>
2026-04-16 15:23:17 -0300

    refactor: move enum EStatusOS para pasta Enums
    - Organiza estrutura de pastas para seguir o padrão DDD do projeto

---
+++ Domain/Enums/EStatusOS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Enums;
+
+public enum EStatusOS
+{
+    Recebida = 1,
+    EmDiagnostico = 2,
+    AguardandoAprovacao = 3,
+    EmExecucao = 4,
+    Finalizada = 5,
+    Entregue = 6,
+    Cancelada = 7
+}
