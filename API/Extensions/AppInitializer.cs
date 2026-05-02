32fb27a68a0f9c0f7914b86a288ab5756a1fc55a
Leonardo Pagni <leo_pagni@hotmail.com>
2026-05-02 13:38:47 -0300

    feat: implementando scalar para documentação de api

---
+++ API/Extensions/AppInitializer.cs
@@ -0,0 +1,56 @@
+﻿using Infra.Context;
+using Infra.Seeds;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
+namespace API.Extensions
+{
+    public static class AppInitializer
+    {
+        public static async Task<WebApplication> UseScalarDocumentation(this WebApplication app)
+        {
+            app.MapOpenApi();
+
+            app.MapScalarApiReference(options =>
+            {
+                options.Title = "Mecanica API";
+                options.Theme = ScalarTheme.DeepSpace;
+                options.DefaultHttpClient = new(ScalarTarget.CSharp, ScalarClient.HttpClient);
+            });
+
+            return app;
+        }
+
+        public static async Task<WebApplication> InitializeDb(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                var retries = 5;
+                while (retries > 0)
+                {
+                    try
+                    {
+                        db.Database.Migrate();
+                        break;
+                    }
+                    catch
+                    {
+                        retries--;
+                        Thread.Sleep(3000);
+                    }
+                }
+            }
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                await DbSeeds.Seed(context);
+            }
+
+            return app;
+        }
+    }
+}
